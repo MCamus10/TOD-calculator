ea045333796ad5ad48d30fd3f653b706b5598b00
MCamus10 <109302595+MCamus10@users.noreply.github.com>
2025-07-09 15:26:55 -0400

    Corrected bug generated after pressing DEL and then trying to add other number to the result on screen

--- js.js
+++ js.js
@@ -1,59 +1,3 @@
-/* 
-if it's number it appends to display and to "a" variable
-if it's operator it sets operator value to the corresponding operation
-if it's "=" it passes the 3 args to the corresponding function and prints result
-if it's "c" it clears display and a, b variables
-if it's "del" it removes last number on screen
-
-a = ""
-b = ""
-operator = ""
-
-BUTTON PRESSED
-    IF IT'S NUMBER
-        APPEND TO DISPLAY
-        IF OPERATOR = ""
-            APPEND TO a AS STRING
-        IF OPERATOR != ""
-            APPEND TO b AS STRING
-    IF IT'S DEL
-        DEL()
-    IF IT'S  OPERATOR AND NOT "C" AND NOT "DEL"
-        OPERATOR = OPERATION
-    ELSE
-        CALL ERROR()
-
-    IF IT'S EQUAL AND operator, AND a, AND b !=""
-        IF a NOT NUMBER
-            CONVERT a TO NUMBER
-        CONVERT b TO NUMBER
-        CALCULATE(operator, a, b)
-            IF OPERATOR = "+" val = ADD(a,b)
-            IF OPERATOR = "-" val = SUB(a,b)
-            IF OPERATOR = "*" val = MULT(a,b)
-            IF OPERATOR = "/" val = DIVIDE(a,b)
-            IF VAL NOT NUMBER
-                ERROR()
-            ELSE 
-                PRINT VAL
-                a = VAL
-                b = ""
-                OPERATOR = ""
-
-DEL()
-    REMOVE LAST ITEM OF SCREEN
-    IF b = ""
-        REMOVE LAST a ITEM
-    IF b != ""
-        REMOVE LAST b ITEM
-
-ERROR()
-    PRINT "ERROR"
-    PRESS C TO ENTER NEW OPERATION
-    RESET a, b, operator
- */
-
-
 let a = "";
 let b = "";
 let operator = "";
@@ -68,9 +12,8 @@ calculator.addEventListener("click", (e) => {
     const clickedButton = e.target
     if (clickedButton.classList.contains("calcKey") && !clickedButton.classList.contains("operator")){
         const value = clickedButton.dataset.value;
-        if (calcValidator == true) {
+        if (calcValidator == true) { //check if a previous calculation has been done
             screen.textContent = "";
-            a = "";
             b = "";
             calcValidator = false;
         };
@@ -93,6 +36,7 @@ calculator.addEventListener("click", (e) => {
         } else if (b != ""){
             a = a.toString().slice(0,-1);
             b = "";
+            calcValidator = false;
         };
 
     } else if (clickedButton.textContent == "C"){
